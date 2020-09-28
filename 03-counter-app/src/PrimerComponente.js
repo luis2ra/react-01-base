ddf35be3e50ff8013513853d298c266c34ea88ae
Luis Altuve <luis2ra@gmail.com>
2020-09-27 20:01:30 -0400

    first steps: probando imprimir una variable en el HTML

--- 03-counter-app/src/PrimerComponente.js
+++ 03-counter-app/src/PrimerComponente.js
@@ -10,10 +10,44 @@
  *
  */
 import React from 'react';
+//import React, { Fragment } from 'react';
 
 const PrimerComponente = () => {
 
-    return <h1>Hola Mundo</h1>;
+    let varMostrar = 'Hola gente';
+    varMostrar = 123;                   // lo muestra OK
+    varMostrar = 123.45;                // lo muestra OK
+    varMostrar = true;                  // no muestra nada ni da error
+    varMostrar = [ 1, 2, 3, 4, 5] ;     // muestra el arreglo contatenado
+
+    /**
+     * El siguiente objeto, inicialmente no lo imprime, da error
+     * Pero si se usa la función JSON.stringify lo convierte en un texto
+     */
+    varMostrar = {
+        nombre: 'Luis',
+        job: 'Developer'
+    }
+
+    return (
+        /**
+         * Forma clasica de definir un Fragment de React
+         */
+        // <Fragment>
+        //     <h1>Hola Mundo</h1>
+        //     <p>Mi primera aplicación</p>
+        // </Fragment>
+
+        /**
+         * Forma simplificada de definir un Fragment
+         */
+        <>
+            <h1>Hola! { JSON.stringify( varMostrar ) } </h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
+    
+    
 }
 
 export default PrimerComponente;
