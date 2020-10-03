62169ca71824747da53c5df00563afdc4d686a30
Luis Altuve <luis2ra@gmail.com>
2020-10-03 16:06:46 -0400

    first steps: task of video 41

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,4 +1,11 @@
-// Condiciones iniciales para desarrollar tarea - video 41
+// Desarrollo de tarea formulada en video 41
+import React from 'react';
+import { render } from 'react-dom';  // render del DOM
+import CounterApp from './CounterApp';
 import './index.css';
 
 console.log('init...');
+
+const divRoot = document.querySelector('#root');
+
+render( <CounterApp value = { 10 } />, divRoot );
